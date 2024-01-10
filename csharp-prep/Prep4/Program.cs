fa36e882efb19fb0ba52af14343a6e51aae16941
Christopher Cowan <soundcommchris@gmail.com>
2024-01-10 11:54:31 -0800

    Prep4 completed

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Collections.Generic;
 
+/*
+Compute the sum, or total, of the numbers in the list.
+
+Compute the average of the numbers in the list.
+
+Find the maximum, or largest, number in the list.
+*/
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        List<int> numbers = new List<int>();
+        int number;
+        int sum = 0;
+        int average = 0;
+        int largest = 0;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0){
+                numbers.Add(number);
+            }
+            
+        }
+        while (number != 0);
+
+        foreach (int item in numbers){
+            sum += item;
+            average = sum / numbers.Count;
+        }
+        for (int i = 0; i < numbers.Count; i++){
+            if(numbers[i] > largest){
+                largest = numbers[i];
+            }
+        }
+        
+        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest number is {largest}");
+
+        
+        
+
+        
+
+
+    } 
 }
