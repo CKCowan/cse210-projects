3376d7edb0ffe252fe1e5d420906b1066d46f239
Christopher Cowan <soundcommchris@gmail.com>
2024-01-29 17:14:57 -0800

    Learning4 completed

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Assignment assignment = new Assignment("Chris", "C#");
+        // Console.WriteLine(assignment.GetSummary());
+
+        // Math math = new Math("Chris", "C#", "7.3", "8-19");
+        // Console.WriteLine(math.GetSummary());
+        // Console.WriteLine(math.GetHomeworkList());
+
+        Writing writing = new Writing("Chris", "Programming Code", "Coding is Fun!");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInfo());
     }
 }
